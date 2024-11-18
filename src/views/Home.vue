5080b1b2e263c66a2faf61412d2bb3e8d8830b4e
Felipe Machado <felipesantos-01@hotmail.com>
2024-11-18 17:23:40 -0300

    PORTAL Restaurante cantos do paladar

---
+++ src/views/Home.vue
@@ -0,0 +1,23 @@
+<template>
+  <Banner />
+  <div class="main-container">
+    <h1>Monte seu burger:</h1>
+    <BurgerForm />
+  </div>
+  <ul>
+      <li v-for="item in data" :key="item.id">{{ item.name }}</li>
+    </ul>
+</template>
+
+<script>
+import Banner from "../components/Banner.vue"
+import BurgerForm from "../components/BurgerForm.vue"
+
+export default {
+  name: "Home",
+  components:{
+    Banner,
+    BurgerForm
+  },
+}
+</script>
